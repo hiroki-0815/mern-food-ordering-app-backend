9ca61693a0651757cdfe09bd4013627e7eb2a4bf
hiroki <haohuirixia851@gmail.com>
2024-12-05 17:59:07 +0900

    Create session API

---
+++ src/routes/OrderRoute.ts
@@ -0,0 +1,9 @@
+import express from "express"
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { createCheckoutSession } from "../controllers/OrderController";
+
+const router = express.Router();
+
+router.post("/checkout/create-checkout-session", jwtCheck,jwtParse, createCheckoutSession)
+
+export default router
