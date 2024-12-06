676d47368cdb485157424f0682c1e9b8041f027f
hiroki <haohuirixia851@gmail.com>
2024-12-06 18:14:57 +0900

    Update order in stripe webhook

--- src/index.ts
+++ src/index.ts
@@ -13,26 +13,30 @@ const PORT = process.env.PORT || 7001;
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
-  .then(() => console.log("Connected to database!"))
-  
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  })
+  .then(() => console.log("Connected to database!"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
 
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
+
 app.get("/health", async (req: Request, res: Response) => {
-  res.send({ message: "Health OK!" });
+  res.send({ message: "health OK!" });
 });
 
 app.use("/api/my/user", myUserRoute);
-app.use("/api/my/restaurant", myRestaurantRoute)
-app.use("/api/restaurant", restaurantRoute)
-app.use("/api/order", orderRoute)
+app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}`);
